e6582a7d63d6f5c53dc4c431da44c71de8d1ad51
KGarg <kapil.garg@sevenlakes.com>
2024-03-02 23:54:37 +0530

    added another implementation

--- house-robber.py
+++ house-robber.py
@@ -1,13 +1,18 @@
+from functools import reduce
 from typing import List
+
 
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        r1=r2=0
+        r1 = r2 = 0
 
         for n in nums:
-            moneyRobbed = max(r2, r1+n)
-            r1=r2
+            moneyRobbed = max(r2, r1 + n)
+            r1 = r2
             r2 = moneyRobbed
 
         return r2
+
+    def rob_2(self, nums: List[int]) -> int:
+        return reduce(lambda a, x: (a[1], max(a[1], a[0] + x)), nums, (0, 0))[1]
