c7ac0f15f31494e506db9da6860fb98e1ee48b0e
KGarg <kapil.garg@sevenlakes.com>
2024-03-02 22:22:25 +0530

    first_commit

---
+++ house-robber-2.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def _rob(nums):
+            r1 = r2 = 0
+            for n in nums:
+                money = max(r1 + n, r2)
+                r1 = r2
+                r2 = money
+            return r2
+
+        return max(nums[0], _rob(nums[:-1]), _rob(nums[1:]))
+
+
+s = Solution()
+r = s.rob([0])
+print(r)
