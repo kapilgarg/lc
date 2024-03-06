afd10115aa9c1cb32dac1b5f6b78c68904c7efd3
Kapil Garg <kapil.garg82@gmail.com>
2024-03-06 23:23:48 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,20 @@ def fib(n):
     if n < 2:
         return n
     return reduce(lambda v, e: (v[1], v[1] + v[0]), range(1, n), (0, 1))[1]
+
+print(timeit('fib(100)', setup="from __main__ import fib "))
+>>>23.8715282
+
+
+# fibonacci using loop
+def fib2(n):
+    a, b = 0, 1
+    if n < 2:
+        return n
+    for _ in range(2, n + 1):
+        nextnum = a + b
+        a, b = b, nextnum
+    return nextnum
+
+print(timeit('fib2(100)', setup="from __main__ import fib2 "))
+>>>8.202182800000003
